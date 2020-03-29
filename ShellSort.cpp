b4ad4f793f0e32577473fbace119458a82a9acd2
yanghao <yheverlove27@outlook.com>
2020-03-29 22:19:28 +0800

    architecture update

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -2,6 +2,7 @@ void ShellSort(int *A,int n) {
 	int i, j, incre;
 
 	for(incre = n / 2; incre > 0; incre /= 2)
+	//距离较远的插入排序
 		for (i = incre; i < n; i++) {
 			int tmp = A[i];
 			for (j = i; j >= incre && A[j - incre] > tmp; j -= incre) 
