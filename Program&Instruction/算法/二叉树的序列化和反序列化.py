53fcc30bcf2f80479fcf295876607f7e350a1d4d
haoy <43663432+daisuki3@users.noreply.github.com>
2021-02-16 13:25:01 +0800

    Update 二叉树的序列化和反序列化.py

--- Program&Instruction/算法/二叉树的序列化和反序列化.py
+++ Program&Instruction/算法/二叉树的序列化和反序列化.py
@@ -86,7 +86,75 @@ class Codec:
         
         return root
     '''
+    '''
+    严格的层次遍历，需要去维护层次状态
+    很多冗余代码，而且序列化字符串里很多None
+    用DFS+队列，不必手动维护层次状态
     
+    这种情况下如何序列化？
+    每次从队列取node，都把node序列化
+    node不为None时，向队列加入node的左右子树
+    队列为空时，序列化完成，因为只有真实node（非None）才能向队列添加元素
+    
+    那么如何反序列化？
+    需要适用队列来把树还原
+    维护一个当前node的孩子指针（不是真的指针，其实就是跟踪data数组的下标）
+    当开始第i层第一个node的处理时，孩子指针在第i+1层的第一个node
+
+    那么对于队列里取出的node
+    真实node（非None）：根据孩子指针更新它的左右子树（注意左右子树为为空时不必更新）
+    ，左右子树入队列，更新孩子指针。
+    虚拟None:什么也不做。因为虚拟node是没有孩子的，它不能去影响孩子指针。
+
+    '''
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if root == None:
+            return ""
+        q = Queue()
+        q.put(root)
+        ans = ""
+        
+        while q.empty() == False:
+            node = q.get()
+            if node == None:
+                ans += ",null"
+            else:
+                ans += "," + str(node.val)
+                q.put(node.left)
+                q.put(node.right)
+        return ans[1:]
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if len(data) == 0:
+            return None
+        data = data.split(",")
+        root = TreeNode(int(data[0]))
+        q = Queue()
+        q.put(root)
+        i = 1
+
+        while q.empty() == False:
+            node = q.get()
+            if node != None:
+                if data[i] != "null":
+                    node.left = TreeNode(int(data[i]))
+                    q.put(node.left)
+                i += 1
+                if data[i] != "null":
+                    node.right = TreeNode(int(data[i]))
+                    q.put(node.right)
+                i += 1
+        return root            
 
             
 
