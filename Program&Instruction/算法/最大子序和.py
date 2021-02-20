e31da0587b8f0bd7f1225478b6c96030e4f114c4
haoy <43663432+daisuki3@users.noreply.github.com>
2021-02-20 21:24:51 +0800

    Create 最大子序和.py

---
+++ Program&Instruction/算法/最大子序和.py
@@ -0,0 +1,25 @@
+'''
+连续子数组的最大和
+
+解法：前缀和，注意变量初始化，以及边界情况的处理
+'''
+class Solution:
+    def maxSubArray(self, nums):
+        if len(nums) == 0:
+            return 0
+
+        s = 0
+        ans = -(10 ** 5)
+        minS = 0
+        
+        for i in range(0, len(nums), 1):
+            s += nums[i]
+            
+            if s - minS > ans:
+                ans = s - minS
+            
+            if s < minS:
+                minS = s
+        
+        return ans
+        
