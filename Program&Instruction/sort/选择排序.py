bed2464d0662e7b6abb05e903f80ac85ec3e2c1c
haoy <43663432+daisuki3@users.noreply.github.com>
2021-02-20 10:52:48 +0800

    Create 选择排序.py

---
+++ Program&Instruction/sort/选择排序.py
@@ -0,0 +1,23 @@
+'''
+复杂度：O(n^2)
+稳定性：每次找最小值时，都是选择第一个最小值，所以维护了稳定性。
+'''
+def SelectionSort(nums):
+    j = 0
+    n = len(nums)
+    while j <= n - 2:
+        minI = j
+        for i in range(j + 1, n):
+            minI = i if nums[i] < nums[minI] else minI
+
+        tmp = nums[j]
+        nums[j] = nums[minI]
+        nums[minI] = tmp
+        j += 1
+    
+
+def main():
+    nums = [3,7,10,1,4, 9,8,5,2,6]
+    SelectionSort(nums)
+    print(nums)
+main()
