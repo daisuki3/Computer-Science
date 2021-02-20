c5f0fe12dc83578850c7de9bbdfdc979ac713565
haoy <43663432+daisuki3@users.noreply.github.com>
2021-02-20 11:03:41 +0800

    Create 插入排序.py

---
+++ Program&Instruction/sort/插入排序.py
@@ -0,0 +1,28 @@
+'''
+复杂度: O(n^2)
+稳定性：插入时只跃过了大于nums[j]的元素，保证了稳定性
+'''
+def InsertionSort(nums):
+    n = len(nums)
+    j = 1
+
+    while j <= n - 1:
+        index = j
+        for i in range(j - 1, -1, -1):
+            if nums[i] <= nums[j]:
+                break
+            elif nums[i] > nums[j]:
+                index = i
+        tmp = nums[j]
+        for i in range(j, index, -1):
+            nums[i] = nums[i - 1]
+        nums[index] = tmp
+
+        j += 1
+    
+
+def main():
+    nums = [3,7,10,1,4, 9,8,5,2,6]
+    InsertionSort(nums)
+    print(nums)
+main()
