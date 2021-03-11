f329427c6c1cfe6d013c8d60447fa5ff58c2a80f
haoy <43663432+daisuki3@users.noreply.github.com>
2021-03-11 22:29:54 +0800

    Update 鸡蛋掉落.py

--- Program&Instruction/算法/鸡蛋掉落.py
+++ Program&Instruction/算法/鸡蛋掉落.py
@@ -1,3 +1,15 @@
+'''
+dp[i][j] = 1 + min( m:1 ~ i max(dp[m - 1][j - 1], dp[i - m][j]) )
+i j 固定时
+dp[m - 1][j - 1] 随m递增
+dp[i - m][j] 随m递减
+两者最大值的最小值，即两个函数图像的交点
+通过遍历可以找到这个交点，但效率低
+
+考虑f(m) =  dp[m - 1][j - 1] - dp[i - 1][j]
+m是一个递增函数，我要找的交点，就是使f(m)在0附件的m值
+递增代表有序，那么可以通过二分来寻找
+'''
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
         dp = [[0] * (k + 1) for _ in range(n + 1)]
@@ -13,7 +25,6 @@ class Solution:
 
         for i in range(1, n + 1):
             for j in range(2, k + 1):
-    
                 # 在1 ~ i的哪个点摔鸡蛋 最优
                 # left dp[m - 1][k - 1] < dp[i - m][k]
                 # right dp[m - 1][k - 1] > dp[i - m][k]
@@ -35,15 +46,5 @@ class Solution:
                 lower_index = left - 1
                 this = max(dp[higher_index - 1][j - 1], dp[i - higher_index][j])
                 pre = max(dp[lower_index - 1][j - 1], dp[i - lower_index][j])
-                
                 dp[i][j] = min(pre, this) + 1
-                '''
-                min_v = -1
-                for m in range(1, i + 1):
-                    #       碎了 在楼下继续找   没碎 在楼上找
-                    v = max(dp[m - 1][k - 1], dp[i - m][k])
-                    if min_v == -1 or v < min_v:
-                        min_v = v 
-                dp[i][j] = min_v + 1
-                '''
         return dp[n][k]
